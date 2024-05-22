2986c5475cc16139bc1fb3f8a24e5ef55fc5198b
dimetrius2000 <dimetris311200@mail.ru>
2024-05-22 22:03:58 +0300

    testovoe

--- app.py
+++ app.py
@@ -1,43 +1,55 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, Response, jsonify, abort
 from typing import Tuple
+from flask import json
+
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
+app.config['JSON_AS_ASCII'] = False
+app.config['JSON_SORT_KEYS'] = False
 
 def data_loader() -> Tuple[dict, dict]:
     """
     Функция загружает данные из json файлов и преобразует их в dict.
     Функция не должна нарушать изначальную структуру данных.
-    """
-    return {}, {}
-
-
-@app.route("/")
-def get_posts():
-    """
-    На странице / вывести json в котором каждый элемент - это:
-    - пост из файла posts.json.
-    - для каждой поста указано кол-во комментариев этого поста из файла comments.json
-
     Формат ответа:
-    posts: [
-        {
-            id: <int>,
-            title: <str>,
-            body: <str>, 
-            author:	<str>,
-            created_at: <str>,
-            comments_count: <int>
-        }
-    ],
-    total_results: <int>
-
-    Порядок ключей словаря в ответе не важен
+#     posts: [
+#         {
+#             id: <int>,
+#             title: <str>,
+#             body: <str>, 
+#             author:  <str>,
+#             created_at: <str>,
+#             comments_count: <int>
+#         }
+#     ],
+#     total_results: <int>
     """
-    posts, comments = data_loader()
-    output = {"body": "Social posts"}
+    with open('data/posts.json', encoding='UTF-8') as f_posts, open('data/comments.json', encoding="UTF-8") as f_comments:
+        posts = json.load(f_posts)
+        comments = json.load(f_comments)
+    return posts, comments
 
-    return jsonify(output)
+@app.route('/')
+def index():
+    posts, comments = data_loader()
+    posts_with_comments_count = []
+    for post in posts["posts"]:
+        comments_count = len([comment for comment in comments["comments"] if comment["post_id"] == post["id"]])
+        post_with_comments_count = {
+            "id": post["id"],
+            "title": post["title"],
+            "body": post["body"],
+            "author": post["author"],
+            "created_at": post["created_at"],
+            "comments_count": comments_count
+        }
+        posts_with_comments_count.append(post_with_comments_count)
+        total_results = f'total_results: {len(posts_with_comments_count)}'
+    
+    return json.jsonify(posts_with_comments_count, total_results)
 
 
 @app.route("/posts/<int:post_id>")
@@ -54,7 +66,7 @@ def get_post(post_id):
     id: <int>,
     title: <str>,
     body: <str>, 
-    author:	<str>,
+    author:  <str>,
     created_at: <str>
     comments: [
         "user": <str>,
@@ -66,6 +78,19 @@ def get_post(post_id):
     Порядок ключей словаря в ответе не важен
     """
     posts, comments = data_loader()
-    output = {"body": "Post: %d" % post_id}
-
+    post = next((post for post in posts["posts"] if post["id"] == post_id), None)
+    if post is None:
+        abort(404)
+    comment = [comment for comment in comments["comments"] if comment['post_id'] == post_id]
+    output = {
+        "id": post["id"],
+        "title": post["title"],
+        "body": post["body"],
+        "author": post["author"],
+        "created_at": post["created_at"],
+        "comments": comment
+    }
     return jsonify(output)
+
+if __name__ == '__main__':
+    app.run()
